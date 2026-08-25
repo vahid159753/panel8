01287eedaa0db88a578ed006a3e5ba173670178f
vahid159753 <kazemi.mymail@gmail.com>
2026-08-25 20:48:58 +0330

    config file changed to dynamic base url

--- application/config/config.php
+++ application/config/config.php
@@ -35,8 +35,10 @@ $config['base_url'] = 'http://192.168.10.15/panel/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
-
+//$config['index_page'] = 'index.php';
+$root  = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = $root;
 /*
 |--------------------------------------------------------------------------
 | URI PROTOCOL
