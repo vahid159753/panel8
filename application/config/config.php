4ae029c9518db8770e20b647e8210c32c29689de
vahid159753 <kazemi.mymail@gmail.com>
2026-08-25 20:52:13 +0330

    re config file changed to dynamic base url pre was wrong

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.10.15/panel/';
+//$config['base_url'] = 'http://192.168.10.15/panel/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://192.168.10.15/panel/';
 | variable so that it is blank.
 |
 */
-//$config['index_page'] = 'index.php';
+$config['index_page'] = 'index.php';
 $root  = "http://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url']    = $root;
